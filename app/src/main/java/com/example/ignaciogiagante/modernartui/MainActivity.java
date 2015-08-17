50c92fd621a739203cf1db0258d50487a9e000b2
Ignacio Giagante <igiagante@gmail.com>
2015-08-17 18:30:24 -0300

    Adding menu with More Information Option.

--- app/src/main/java/com/example/ignaciogiagante/modernartui/MainActivity.java
+++ app/src/main/java/com/example/ignaciogiagante/modernartui/MainActivity.java
@@ -1,27 +1,44 @@
 package com.example.ignaciogiagante.modernartui;
 
-import android.app.Activity;
+import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends Activity implements SeekBarFragment.onSeekChangedListener {
+public class MainActivity extends AppCompatActivity implements SeekBarFragment.onSeekChangedListener {
 
-    private GalleryFragment galleryFragment;
+    private GalleryFragment mGalleryFragment;
+    private DialogFragment mDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ActionBar actionBar = getActionBar();
+        if(actionBar != null){
+            actionBar.show();
+        }
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -32,8 +49,11 @@ public class MainActivity extends Activity implements SeekBarFragment.onSeekChan
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.more_information) {
+            // Create a new AlertDialogFragment
+            mDialog = AlertDialogFragment.newInstance();
+            // Show AlertDialogFragment
+            mDialog.show(getFragmentManager(), "Alert");
         }
 
         return super.onOptionsItemSelected(item);
@@ -41,10 +61,50 @@ public class MainActivity extends Activity implements SeekBarFragment.onSeekChan
 
     @Override
     public void updateColors(int value) {
-       galleryFragment = (GalleryFragment) getFragmentManager().findFragmentById(R.id.gallery_fragment);
+       mGalleryFragment = (GalleryFragment) getFragmentManager().findFragmentById(R.id.gallery_fragment);
 
-        if(galleryFragment != null){
-            galleryFragment.updateColors(value);
+        if(mGalleryFragment != null){
+            mGalleryFragment.updateColors(value);
+        }
+    }
+
+    public static class AlertDialogFragment extends DialogFragment {
+
+        public static AlertDialogFragment newInstance(){
+            return new AlertDialogFragment();
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+            TextView myMsg = new TextView(getActivity());
+            myMsg.setText("Inspired by the works of artists such as \n" +
+                    "Check below to learn more!");
+            myMsg.setGravity(Gravity.CENTER_HORIZONTAL);
+            myMsg.setTextSize(20);
+            myMsg.setPadding(5, 5, 5, 5);
+            myMsg.setTextColor(Color.BLACK);
+
+            return new AlertDialog.Builder(getActivity())
+                    .setView(myMsg)
+
+                    .setCancelable(true)
+
+                    .setNegativeButton("Not Now", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+
+                    .setPositiveButton("Visit MOMA", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.moma.org/"));
+                            startActivity(browserIntent);
+                            dialog.dismiss();
+                        }
+                    }).create();
         }
     }
 }
